19e6549e54f79d1a85b0db2f6558e65f248d2c6a
Ross Marshall <rossmarshallgithub@gmail.com>
2017-06-05 00:12:09 -0400

    To Step 3

--- SportsEngineAPI/api_test.py
+++ SportsEngineAPI/api_test.py
@@ -2,6 +2,8 @@ import httplib
 #from http.client import HTTPSConnection
 from base64 import b64encode
 import sys
+import urllib
+import ast
 
 #
 # Get the variables from the properties file.
@@ -34,8 +36,7 @@ c = httplib.HTTPSConnection( url )
 
 #we need to base 64 encode it 
 #and then decode it to acsii as python 3 stores it as a byte string
-userAndPass = b64encode(b"blahblah:blahblah").decode("ascii")
-#userAndPass = b64encode( username + ':' + password.encode('utf-8')  ).decode("ascii")
+userAndPass = b64encode( username + ':' + password.encode('utf-8')  ).decode("ascii")
 
 print 'userAndPass [' + userAndPass + ']'
 headers = { 'Authorization' : 'Basic %s' %  userAndPass }
@@ -47,6 +48,7 @@ headers = { 'Authorization' : 'Basic %s' %  userAndPass }
 
 login_url = '/oauth/authorize?response_type=code&client_id=[CLIENT_IDENTIFIER]&redirect_uri=[REDIRECT_URI]'
 login_url = login_url.replace( '[CLIENT_IDENTIFIER]', client_id ).replace( '[REDIRECT_URI]', redirect_url ) 
+print 'url       [' + url + ']'
 print 'login_url [' + login_url + ']'
 #login_url = login_url.replace( client_template, client_id ).replace( redirect_template, redirect_url )
 print 'login_url [' + login_url + ']'
@@ -55,25 +57,73 @@ print '-----------------------------------------------------'
 #then connect
 c.request('GET', login_url, headers=headers)
 
-
 #get the response back
 res = c.getresponse()
 
 print 'response ====> ' + str( res )
+print 'Request after Step 1 POST getheader : ' + str( res.getheader )
+print 'Request after Step 1 POST msg       : ' + str( res.msg )
+print 'Request after Step 1 POST verson    : ' + str( res.version )
+print 'Request after Step 1 POST status    : ' + str( res.status )
+print 'Request after Step 1 POST reason    : ' + str( res.reason )
+#print 'Request after Step 1 POST read()    : ' + str( res.read() )
 
 # at this point you could check the status etc
 # this gets the page text
 data = res.read() 
 print 'Login data: ' + data
+print 'Data 1 : data ====> ' + str( data )
 
 code=data.split('=')[2].split('"')[0]
 print 'code = ' + code
-sys.exit(0)
 
 #access_url='https://user.sportngin.com/oauth/token?grant_type=authorization_code&client_id=[CLIENT_IDENTIFIER]&client_secret=[CLIENT_SECRET]&code=[AUTHORIZATION CODE]'.replace( '[CLIENT_IDENTIFIER]', client_id ).replace( '[CLIENT_SECRET]', secret ).replace( '[AUTHORIZATION CODE]', code )
-access_url=access_url.replace( client_template, client_id ).replace( secret_template, secret ).replace( authorization_template, code )
+access_url='/oauth/token?grant_type=authorization_code&client_id=[CLIENT_IDENTIFIER]&client_secret=[CLIENT_SECRET]&code=[AUTHORIZATION CODE]'.replace( '[CLIENT_IDENTIFIER]', client_id ).replace( '[CLIENT_SECRET]', secret ).replace( '[AUTHORIZATION CODE]', code )
+access_url2=access_url.replace( client_template, client_id ).replace( secret_template, secret ).replace( authorization_template, code )
 
-print 'access_url = ' + access_url
+print 'access_url  = ' + access_url
+print 'access_url2 = ' + access_url2
 
-c.request('POST', access_url)
-print c.getresponse().read()
+params = urllib.urlencode({'@number': 12524, '@type': 'issue', '@action': 'show'})
+headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+
+#c.request('POST', access_url)
+c.request('POST', access_url, params, headers)
+res = c.getresponse()
+print 'Request after Step 2 POST getheaders: ' + str( res.getheaders() )
+print 'Request after Step 2 POST msg       : ' + str( res.msg )
+print 'Request after Step 2 POST verson    : ' + str( res.version )
+print 'Request after Step 2 POST status    : ' + str( res.status )
+print 'Request after Step 2 POST reason    : ' + str( res.reason )
+data_str = res.read()
+print str( type(data)  )
+data = ast.literal_eval( data_str )
+print 'Data 2 : data ====> ' + str( data )
+  
+access_token = data['access_token']
+refresh_token = data['refresh_token']
+
+print 'access_token  : ' + access_token
+print 'refresh_token : ' + refresh_token
+
+test_access_url='/oauth/me?access_token="' + access_token + '"'
+print 'test_access_url : ' + test_access_url
+ 
+headers = { 'Authorization' : 'Basic %s' %  userAndPass }
+c.request('GET', test_access_url, headers=headers)
+
+#get the response back
+res = c.getresponse()
+
+print 'response ====> ' + str( res )
+print 'Request after Step 3 GET getheader : ' + str( res.getheaders() )
+print 'Request after Step 3 GET msg       : ' + str( res.msg )
+print 'Request after Step 3 GET verson    : ' + str( res.version )
+print 'Request after Step 3 GET status    : ' + str( res.status )
+print 'Request after Step 3 GET reason    : ' + str( res.reason )
+data_str = res.read()
+print str( type(data_str)  )
+print data_str
+#data = ast.literal_eval( data_str )
+#print 'Data 3 : data ====> ' + str( data )
+
